9160dc3064d1fc69d7fbaa9c5a9f491934695ca2
dev-ansh-r <rdevanshshukla26@gmail.com>
2024-08-26 13:03:24 +0530

    real robot working

---
+++ launch/launch_robot.launch.py
@@ -0,0 +1,70 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command
+from launch_ros.actions import Node
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessStart
+
+def generate_launch_description():
+    # Define the package name
+    package_name = 'chunkbot_h'
+
+    # Include the Robot State Publisher to publish the robot state
+    rsp = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(package_name), 'launch', 'rsp.launch.py'
+        )]),
+        launch_arguments={'use_sim_time': 'false'}.items()
+    )
+
+
+    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+
+    controller_params_file = os.path.join(get_package_share_directory(package_name), 'config', 'my_controllers.yaml')
+
+    controller_manager = Node(
+        package='controller_manager',
+        executable='rso2_control_node',
+        parameters=[{'robot_description': robot_description}, controller_params_file] # Add parameters here
+    )
+    
+    delayed_controller_manager = TimerAction(
+        period=3.0,
+        actions=[controller_manager],
+    )
+
+    diff_drive_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['diff_cont'],
+    )
+    delayed_diff_drive_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_exit=[diff_drive_spawner],
+        )
+    )
+
+    joint_broad_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['joint_broad'],
+    )
+    delayed_joint_broad_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[joint_broad_spawner],
+        )
+    )
+
+    return LaunchDescription([
+        rsp,
+        delayed_controller_manager,
+        delayed_diff_drive_spawner,
+        delayed_joint_broad_spawner,
+    ])
